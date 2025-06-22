c33da21412b58f56b313d462d1c3cf7e4cf27568
rohingarg12 <rohin.garg12@gmail.com>
2025-06-22 23:45:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/frontent/', // your repo name with trailing slash
+  base: '/frontent2/',  // repo name with slash
   plugins: [react()]
 })
